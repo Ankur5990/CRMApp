b244648d2b89497d2a4949cd2df6dc72049faa25
Ankur5990 <ankur5990@gmail.com>
2021-06-03 16:28:52 +0530

    Added reports service file

---
+++ src/app/pages/reports/reports.service.ts
@@ -0,0 +1,32 @@
+import { map} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { environment } from 'environments/environment';
+
+
+@Injectable()
+
+export class ReportsService {
+  reportTask: any = {};
+  API_ENDPOINT = environment.apiEndpoint;
+  constructor(private http: HttpClient) {
+  }
+  
+  getReportsObject(): any {
+    return this.reportTask;
+  }
+
+  getAllReportTypes(UserId){
+    return this.http.get(`${this.API_ENDPOINT}api/ReportList?UserID=${UserId}`).pipe(map( res => res));
+  }
+
+  getReportData(obj, flag) {
+    let apiPath = '';
+    if(flag == true) {
+      apiPath = `api/ReportData?ReportType=${obj.ReportType}&LoginUserID=${obj.userID}`
+    } else {
+      apiPath = `api/ReportData?ReportType=${obj.ReportType}&FromDate=${obj.StartDate}&EndDate=${obj.EndDate}&LoginUserID=${obj.userID}`
+    }
+    return this.http.get(`${this.API_ENDPOINT}${apiPath}`).pipe(map( res => res));
+  }
+}
